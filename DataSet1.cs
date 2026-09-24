abf22f94f0729a94ac8d5d88ddb6b8445cb26f9d
nabil101012 <eng.nabil1010@gmail.com>
2026-09-24 23:49:48 +0300

    1

---
+++ DataSet1.cs
@@ -0,0 +1,130 @@
+﻿namespace ERP
+{
+    
+    
+    public partial class DataSet1 {
+        partial class result_sheet_data2DataTable
+        {
+        }
+    
+        partial class result_sheet_dataDataTable
+        {
+        }
+        partial class graduation_shahadaDataTable
+        {
+        }
+    
+        partial class allinvoiceDataTable
+        {
+        }
+    
+        partial class allmoneyDataTable
+        {
+        }
+    
+        partial class all_invoice_searchDataTable
+        {
+        }
+    
+        partial class subjects_registrationDataTable
+        {
+        }
+        partial class exam_employeeDataTable
+        {
+        }
+    
+        partial class students_examDataTable
+        {
+       }
+    
+        partial class time_intervalDataTable
+        {
+        }
+    
+        partial class carneehDataTable
+        {
+        }
+    
+        partial class flagsDataTable
+        {
+        }
+
+        partial class result_infoDataTable
+        { }
+        partial class late_moneyDataTable
+        {
+        }
+    
+        partial class course_statsticsDataTable
+        {
+        }
+    
+        partial class entry_course_statusDataTable
+        {
+        }
+    
+        partial class result_natiga_testDataTable
+        {
+        }
+    
+        partial class mid_morag3aDataTable
+        {
+       }
+    
+        partial class first_studentsDataTable
+        {
+        }
+    
+        partial class result_info_testDataTable
+        {
+     }
+    
+        partial class semsterDataTable
+        {
+        }
+    
+        partial class student_registrationDataTable
+        {
+        }
+    
+        partial class student_registraion_infoDataTable
+        {
+        }
+    
+        partial class subjects_dataDataTable
+        {
+        }
+    
+        partial class subjects_studentsDataTable
+        {
+        }
+    
+        partial class resultDataTable
+        {
+        }
+
+        partial class result_info_test2DataTable
+        {
+        }
+    
+        partial class studentDataTable
+        {
+       }
+    
+        partial class course_result1DataTable
+        {
+        }
+    
+        partial class yearsDataTable
+        {
+        }
+    
+        partial class tagneed_infoDataTable
+        {
+        }
+    
+        partial class kaidDataTable
+        {
+        }
+    }
+}
